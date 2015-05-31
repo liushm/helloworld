3ed024e17902d62a9db072b3431d5c58fe7eb258
liushm <liushm.cn@gmail.com>
2015-05-31 18:45:31 +0900

    C operator priorities

--- main.cc
+++ main.cc
@@ -7,7 +7,19 @@ int main(int argc, char *argv[]) {
         std::cout << message << std::endl;
     };
 
+	auto proc = [str = message] {
+		std::cout << str << std::endl;
+	};
+
     func();
+	proc();
+
+	std::cout << ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ sizeof func << std::endl;
+	std::cout << *&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*& message << std::endl;
+
+	std::cout << *std::string(message).c_str() << std::endl;
+
+	std::cout << * (1 + reinterpret_cast<int *>(& func)) << std::endl;
 
     return 0;
 }
